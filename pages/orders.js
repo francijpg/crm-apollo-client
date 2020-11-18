db41ac377323ea2c27e06a3af9e0cf5fb9f1c0f3
francijpg <hellofrancijpg@gmail.com>
2020-11-18 10:43:38 -0300

    Create orders.js

---
+++ pages/orders.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Orders = () => {
+  return (
+    <>
+      <Layout>
+        <h1 className="text-2xl text-gray-800 font-light">Orders</h1>
+      </Layout>
+    </>
+  );
+};
+
+export default Orders;
