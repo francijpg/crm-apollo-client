dc79fcef984ccecc5da6aa277648bc84a56bc0b1
francijpg <hellofrancijpg@gmail.com>
2020-11-18 10:31:47 -0300

    Create register.js

---
+++ pages/register.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Register = () => {
+  return (
+    <div>
+      <Layout>
+        <h1 className="text-2xl text-gray-800 font-light">Register</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Register;
