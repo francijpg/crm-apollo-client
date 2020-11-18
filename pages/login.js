c967c661a170466dd629780581f49a7d504162a2
francijpg <hellofrancijpg@gmail.com>
2020-11-18 10:30:55 -0300

    Create login.js

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Login = () => {
+  return (
+    <div>
+      <Layout>
+        <h1 className="text-2xl text-gray-800 font-light">Login</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Login;
