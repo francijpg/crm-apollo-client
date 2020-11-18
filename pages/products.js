820322d1fbf131c57fc78ad222cc3dbf704b6585
francijpg <hellofrancijpg@gmail.com>
2020-11-18 10:30:51 -0300

    Create products.js

---
+++ pages/products.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Products = () => {
+  return (
+    <div>
+      <Layout>
+        <h1 className="text-2xl text-gray-800 font-light">Products</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Products;
