05830858fe44d2fc9ad52cff75cd3a40b62ab81e
francijpg <hellofrancijpg@gmail.com>
2020-11-19 10:44:36 -0300

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,7 +9,7 @@ const Sidebar = () => {
   return (
     <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-5">
       <div>
-        <p className="text-white text-2xl font-black">CRM Clientes</p>
+        <p className="text-white text-2xl font-black">CRM Clients</p>
       </div>
 
       <nav className="mt-5 list-none">
@@ -31,7 +31,7 @@ const Sidebar = () => {
           }
         >
           <Link href="/products">
-            <a className="text-white block">Productos</a>
+            <a className="text-white block">Products</a>
           </Link>
         </li>
       </nav>
