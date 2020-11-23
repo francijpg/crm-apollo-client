41271ed0e7d44938d59dabb16f6027ac72a615d7
francijpg <hellofrancijpg@gmail.com>
2020-11-23 11:52:44 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -18,10 +18,14 @@ const Header = () => {
   // query de apollo
   const { data, loading, error, client } = useQuery(OBTENER_USUARIO);
 
+  // console.log(data)
+  // console.log(loading)
+  // console.log(error)
+
   // Proteger que no accedamos a data antes de tener resultados
   if (loading) return null;
 
-  // Si no hay informacion
+  // Si no hay informacion (sin token o token no valido), redirigir al login
   if (!data) {
     return router.push("/login");
   }
@@ -29,15 +33,20 @@ const Header = () => {
   const { nombre, apellido } = data.obtenerUsuario;
 
   const cerrarSesion = () => {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     client.clearStore();
-    router.push('/login');
+    router.push("/login");
   };
+
+  const capitalize = (str, lower = false) =>
+    (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
+      match.toUpperCase()
+    );
 
   return (
     <div className="sm:flex sm:justify-between mb-6">
       <p className="mr-2 mb-5 lg:mb-0">
-        Hola: {nombre} {apellido}
+        Hola: {capitalize(nombre)} {capitalize(apellido)}
       </p>
 
       <button
