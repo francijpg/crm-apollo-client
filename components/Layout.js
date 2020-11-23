ade06180156315accbe1c2465e5a84cad12bdc95
francijpg <hellofrancijpg@gmail.com>
2020-11-23 11:52:52 -0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,31 +1,33 @@
 import React from "react";
 import Head from "next/head";
 import Sidebar from "./Sidebar";
+import { useRouter } from "next/router";
+import Header from "../components/Header";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+
   return (
     <>
       <Head>
         <title>CRM - Clients Admin</title>
-        {/* <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-          integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU="
-          crossOrigin="anonymous"
-        /> */}
-        {/* <link
-          href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
-          rel="stylesheet"
-        /> */}
       </Head>
-      <div className="bg-gray-200 min-h-screen">
-        <div className="sm:flex min-h-screen">
-          <Sidebar />
-          <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
-            {children}
-          </main>
+      {router.pathname === "/login" || router.pathname === "/register" ? (
+        <div className="bg-gray-800 min-h-screen flex flex-col justify-center">
+          <div>{children}</div>
         </div>
-      </div>
+      ) : (
+        <div className="bg-gray-200 min-h-screen">
+          <div className="sm:flex min-h-screen">
+            <Sidebar />
+            
+            <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+              <Header />
+              {children}
+            </main>
+          </div>
+        </div>
+      )}
     </>
   );
 };
