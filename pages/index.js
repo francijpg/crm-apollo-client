531cedb9d3e5bbb5fc33c32acdccca1fb233d0a6
francijpg <hellofrancijpg@gmail.com>
2020-11-24 09:20:24 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,66 @@
-import React from "react";
 import Layout from "../components/Layout";
+import Cliente from "../components/Cliente";
+import { gql, useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Spinner from "../components/Spinner";
+
+const OBTENER_CLIENTES_USUARIO = gql`
+  query obtenerClientesVendedor {
+    obtenerClientesVendedor {
+      id
+      nombre
+      apellido
+      empresa
+      email
+    }
+  }
+`;
 
 const Index = () => {
+  const router = useRouter();
+
+  // Consulta de Apollo
+  const { data, loading, error, client } = useQuery(OBTENER_CLIENTES_USUARIO);
+
+  // if (loading) return "Loading....";
+  if (loading) return <Spinner />;
+
+  if (!data.obtenerClientesVendedor) {
+    client.clearStore();
+    router.push('/login');
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <Layout>
         <h1 className="text-2xl text-gray-800 font-light">Clients</h1>
+        <Link href="/newclient">
+          <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold w-full lg:w-auto text-center">
+            New Client
+          </a>
+        </Link>
+
+        <div className="overflow-x-scroll">
+          <table className="table-auto shadow-md mt-10 w-full w-lg">
+            <thead className="bg-gray-800">
+              <tr className="text-white">
+                <th className="w-1/5 py-2">Name</th>
+                <th className="w-1/5 py-2">Company</th>
+                <th className="w-1/5 py-2">Email</th>
+                <th className="w-1/5 py-2">Delete</th>
+                <th className="w-1/5 py-2">Edit</th>
+              </tr>
+            </thead>
+
+            <tbody className="bg-white">
+              {data.obtenerClientesVendedor.map((cliente) => (
+                <Cliente key={cliente.id} cliente={cliente} />
+              ))}
+            </tbody>
+          </table>
+        </div>
       </Layout>
     </div>
   );
